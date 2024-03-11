d823bd38b9c6fbe7622635ad418b9453a2e59fa3
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-11 14:53:33 -0400

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 == 0] or []
+
+    # result = []
+    # for n in num_list:
+    #     if n % 2 == 0:
+    #         result.append(n)
+    # return result
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list] or []
